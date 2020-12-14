e66403897dee55d93993cb2add5f96973705c40f
Ruslan <arraylist@yandex.ru>
2020-12-14 17:33:20 +0300

    response callback added

--- sendRequest.js
+++ sendRequest.js
@@ -1,7 +1,6 @@
-
 //Send a request
 //Must send methid as a string, url as a string, data as an object, headers as an array of objects
-window.sendRequest = (method,url,data,headers) =>{
+window.sendRequest = (method,url,data,headers,onResponse) =>{
     let xhr = new XMLHttpRequest();
     
     let json = JSON.stringify(data);
@@ -23,6 +22,12 @@ window.sendRequest = (method,url,data,headers) =>{
         if (xhr.status != 200) { // analyze HTTP status of the response
           console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
         } 
+        if (onResponse && typeof onResponse === 'function'){
+            onResponce({
+                status:xhr.status,
+                statusText: xhr.statusText
+            })
+        }
         return;
       };
 
